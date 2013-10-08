39183bd8063b4e12f533b599f4657968a9ee72c2
edubecks <edubecks007@gmail.com>
2013-10-08 10:58:08 -0300

    new: carona model

--- djangoapp/caronasbrasil/models.py
+++ djangoapp/caronasbrasil/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Carona(models.Model):
+    origin = models.CharField(max_length=30)
+    destiny = models.CharField(max_length=30)
+    date = models.DateTimeField()
+    ofereco_procuro = models.CharField(max_length=1) ## o: ofereco, p: procuro
+    num_vagas = models.PositiveSmallIntegerField()
