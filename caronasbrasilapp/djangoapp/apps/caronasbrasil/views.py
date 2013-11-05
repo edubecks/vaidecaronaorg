a9d50f13f62930828d6cf48eeb15f9d3336298e0
edubecks <edubecks007@gmail.com>
2013-11-05 10:29:06 -0200

    removing facebook social

--- caronasbrasilapp/djangoapp/apps/caronasbrasil/views.py
+++ caronasbrasilapp/djangoapp/apps/caronasbrasil/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response, render, redirect
 from django.http import *
 from django.core.context_processors import csrf
 from django.template import RequestContext
-from social.backends.facebook import FacebookAppOAuth2
+# from social.backends.facebook import FacebookAppOAuth2
 from unidecode import unidecode
 from django.conf import settings
 from djangoapp.apps.caronasbrasil.main_controller import MainController
@@ -88,31 +88,32 @@ def search(request, op, from_city, to_city, date, from_time, to_time):
         RequestContext(request)
     )
 
-@login_required
-def done(request):
-    """Login complete view, displays user data"""
-    scope = ' '.join(FacebookAppOAuth2.DEFAULT_SCOPE)
-    print scope
-    return render_to_response('index.html', {
-        'user': request.user,
-        'plus_id': getattr(settings, 'SOCIAL_AUTH_GOOGLE_PLUS_KEY', None),
-        'plus_scope': scope
-    }, RequestContext(request))
-
-
-def signup_email(request):
-    return render_to_response('email_signup.html', {}, RequestContext(request))
-
-
-def validation_sent(request):
-    return render_to_response('validation_sent.html', {
-        'email': request.session.get('email_validation_address')
-    }, RequestContext(request))
-
-
-def require_email(request):
-    if request.method == 'POST':
-        request.session['saved_email'] = request.POST.get('email')
-        backend = request.session['partial_pipeline']['backend']
-        return redirect('social:complete', backend=backend)
-    return render_to_response('email.html', RequestContext(request))
+#
+# @login_required
+# def done(request):
+#     """Login complete view, displays user data"""
+#     scope = ' '.join(FacebookAppOAuth2.DEFAULT_SCOPE)
+#     print scope
+#     return render_to_response('index.html', {
+#         'user': request.user,
+#         'plus_id': getattr(settings, 'SOCIAL_AUTH_GOOGLE_PLUS_KEY', None),
+#         'plus_scope': scope
+#     }, RequestContext(request))
+#
+#
+# def signup_email(request):
+#     return render_to_response('email_signup.html', {}, RequestContext(request))
+#
+#
+# def validation_sent(request):
+#     return render_to_response('validation_sent.html', {
+#         'email': request.session.get('email_validation_address')
+#     }, RequestContext(request))
+#
+#
+# def require_email(request):
+#     if request.method == 'POST':
+#         request.session['saved_email'] = request.POST.get('email')
+#         backend = request.session['partial_pipeline']['backend']
+#         return redirect('social:complete', backend=backend)
+#     return render_to_response('email.html', RequestContext(request))
